6f47e1ca2ddc6ee351ce119ca0b8f3337b876338
BOTJEFFreal <74595339+BOTJEFFreal@users.noreply.github.com>
2023-07-13 16:18:32 +0530

    fixed input tag padding and cateringpage subheading content

--- website-clone/src/pages/catering/catering.js
+++ website-clone/src/pages/catering/catering.js
@@ -12,8 +12,9 @@ function CateringPage() {
         <div class="main-heading">
           <h1>Careers</h1>
           <div class="sub-heading">
-            We are hiring! Apply below to become a part of our awesome team and
-            we'll get back to you ASAP!
+            Please fill out the form below and we'll do our best to accommodate
+            your desired date and any requests you may have to make your
+            party/celebration memorable.
           </div>
         </div>
         <div className="form">
@@ -23,7 +24,7 @@ function CateringPage() {
             <input type="checkbox" className="checkbox-polices"></input>
             <label>
               I'd like to get texts about specials, events, and other exclusive
-              offers and announcements not available to the general public. 
+              offers and announcements not available to the general public.
               <a href="https://www.spothopperapp.com/spots/77510/privacy_policy?_gl=1*1xz9hyu*_ga*NTAxMzgzNTk1LjE2ODg1Nzg0Mjc.*_ga_X1WPXLW71C*MTY4ODgxODUyNS40LjEuMTY4ODgyMTE3NS4wLjAuMA..*_ga_VG24VK2VKT*MTY4ODgxODUxMC45LjEuMTY4ODgyMTE3NS4wLjAuMA..*_ga_31HXNESSJY*MTY4ODgyMTE3My4xMC4wLjE2ODg4MjExNzUuMC4wLjA.&amp;_ga=2.114276871.1292730737.1688680247-501383595.1688578427">
                 Privacy Policy
               </a>
