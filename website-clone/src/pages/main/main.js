eb673f34eb3ab6a4eb6f219ebfd186ddea4322d4
BOTJEFFreal <74595339+BOTJEFFreal@users.noreply.github.com>
2023-07-16 22:16:55 +0530

    added video to mainpage

--- website-clone/src/pages/main/main.js
+++ website-clone/src/pages/main/main.js
@@ -3,43 +3,64 @@ import Header from "../../components/header/header";
 import CardsComponents from "./components/cards/cards";
 
 import Footer from "../../components/footer/footer";
-import Imagegrid from "../../components/imagegrid/imagegrid"
+import Imagegrid from "../../components/imagegrid/imagegrid";
 import partiesImg from "./components/cards/parties.png";
 import cateringImg from "./components/cards/catering.png";
 import AboutUsCardComponent from "./components/aboutUScard/aboutUscard";
 import MapComponent from "./components/map/map";
-import Reservation from "./components/reservation/reservation"
+import Reservation from "./components/reservation/reservation";
 // import borderImage from './border.svg';
 // import borderImage2 from './Vector.svg';
-import '@fortawesome/fontawesome-free/css/all.css';
-
+import "@fortawesome/fontawesome-free/css/all.css";
+import video from './video.mp4'
 
 function MainPage() {
   return (
     <div className="main-page">
       <Header></Header>
       <div className="main-content">
-     
-      <div className="red-banner">
-        <h1>
-          <a href="#" className="location-pin">
-            <i className="fa fa-map-marker"></i>
-          </a>
-          2620 Regatta Drive, Las Vegas, NV 89128 
-        </h1>
-      </div>
-      <AboutUsCardComponent/>
-      <CardsComponents toggle={true} heading="Catering" subheading="Let us cater your next event" content=" We Cater your Needs. Elegant style of catering for your style
-                and budget. Any kind of event from weddings to birthdays." img={cateringImg}/>
-      <CardsComponents toggle={false} heading="Parties" subheading="Book your next party with us!" content="No matter the occasion you can celebrate at our place! Book a private party." img={partiesImg}/>
-      {/* <img className="imggg" src ={borderImage} alt="My Happy SVG"/> */}
-      <Reservation/>
-      <Imagegrid/>
-      <MapComponent/>
+        <div className="video-player">
+        <video autoplay loop muted>
+          <source
+            src={video}
+          ></source>
+          Your browser does not support the video tag.
+        </video>
+        </div>
+       
+
+        <div className="red-banner">
+          <h1>
+            <a href="#" className="location-pin">
+              <i className="fa fa-map-marker"></i>
+            </a>
+            2620 Regatta Drive, Las Vegas, NV 89128
+          </h1>
+        </div>
+        <AboutUsCardComponent />
+        <CardsComponents
+          toggle={true}
+          heading="Catering"
+          subheading="Let us cater your next event"
+          content=" We Cater your Needs. Elegant style of catering for your style
+                and budget. Any kind of event from weddings to birthdays."
+          img={cateringImg}
+        />
+        <CardsComponents
+          toggle={false}
+          heading="Parties"
+          subheading="Book your next party with us!"
+          content="No matter the occasion you can celebrate at our place! Book a private party."
+          img={partiesImg}
+        />
+        {/* <img className="imggg" src ={borderImage} alt="My Happy SVG"/> */}
+        <Reservation />
+        <Imagegrid />
+        <MapComponent />
       </div>
       {/* <video autoplay="autoplay" id="coverVideo" loop="loop" muted="muted" playsinline="playsinline" poster="https://static.spotapps.co/web/americanalasvegas--com/custom/video_poster.jpg" data-vscid="td6vs1676">
             <source src="https://static.spotapps.co/website_videos/Americana_Website_Intro_EditedVideo_01142022_Vimeo720p30.m4v" type="video/mp4"></source></video>*/}
-      
+
       <Footer></Footer>
     </div>
   );
