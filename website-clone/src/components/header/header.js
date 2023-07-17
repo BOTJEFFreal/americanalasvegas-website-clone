245ff57547ae97b43d138beb6da333c9dd17b994
BOTJEFFreal <74595339+BOTJEFFreal@users.noreply.github.com>
2023-07-17 14:17:27 +0530

    added view for header for items under 766px width

--- website-clone/src/components/header/header.js
+++ website-clone/src/components/header/header.js
@@ -1,12 +1,20 @@
 import "./header.css";
+import "./burger.css";
 import { useEffect, useState } from "react";
 import MyClickableImage from "../imagetag";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faYelp } from "@fortawesome/free-brands-svg-icons";
 
-function Header() {
+
+
+
+function Header() { 
+
   const [isScrolled, setIsScrolled] = useState(false);
+  const [burger_class, setBurgerClass] = useState("burger-bar unclicked");
+  const [menu_class, setMenuClass] = useState("menu hidden");
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -20,6 +28,19 @@ function Header() {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  // toggle burger menu change
+  const updateMenu = () => {
+    if (!isMenuClicked) {
+      setBurgerClass("burger-bar clicked");
+      setMenuClass("menu visible");
+    } else {
+      setBurgerClass("burger-bar unclicked");
+      setMenuClass("menu hidden");
+    }
+    setIsMenuClicked(!isMenuClicked);
+  };
+
   return (
     <>
       {/* <div className={`${isScrolled ? "header" : "header"}`}> */}
@@ -27,6 +48,7 @@ function Header() {
 
         <img className={`logo-image ${isScrolled ? "scrolled" : ""}`} src="https://web.archive.org/web/20230328001623im_/https://static.spotapps.co/web/americanalasvegas--com/custom/logo.png"></img>
         <div className="header-right">
+          <div className="header-tags"></div>
           <a href="#contact">MENU</a>
           <a href="#about">DRINKS</a>
           <a href="#contact">CATERING</a>
@@ -37,11 +59,37 @@ function Header() {
 
           <FontAwesomeIcon className="icon" icon={faInstagram} />
           <FontAwesomeIcon className="icon" icon={faYelp} />
-         
+           <div className="burger-menu" onClick={updateMenu}>
+          <div className={burger_class}></div>
+          <div className={burger_class}></div>
+          <div className={burger_class}></div>
+        </div>
         </div>
       </nav>
+      <div className={menu_class}>
+        <button className="nav-container defalut-select">Menu</button>
+        <button className="nav-container">Drinks</button>
+        <button className="nav-container">Catering</button>
+        <button className="nav-container">Events</button>
+        <button className="nav-container">Parties</button>
+        <button className="nav-container">Reserve</button>
+        <button className="nav-container">Jobs</button>
+      </div>
+      <footer className="bottom-nav">
+        <div className="bottom-nav-button">Reserve</div>
+        <div className="bottom-nav-button">Jobs</div>
+        <div className="bottom-nav-button">Parties</div>
+        <div className="bottom-nav-button">Catering</div>
+        </footer>  
+               
+
     </>
   );
 }
 
+
+
+
 export default Header;
+
+
